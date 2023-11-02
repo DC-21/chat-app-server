8e7f17adc6ef958fdeabe1e8b05c14275a2d6f19
DC-21 <cholahkuboko@gmail.com>
2023-11-02 22:48:42 +0200

    first commit

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,10 +20,17 @@ app.use((0, body_parser_1.urlencoded)({
     extended: true
 }));
 app.use((0, body_parser_1.json)());
-const start = () => {
-    mongoose_1.default.connect(process.env.MONGO_URI);
-};
-start();
-app.listen(8000, () => {
-    console.log("server is running on port 8k");
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    if (!process.env.MONGO_URI)
+        throw new Error('MONGO_URI is required');
+    try {
+        yield mongoose_1.default.connect(process.env.MONGO_URI);
+    }
+    catch (err) {
+        throw new Error('database error!');
+    }
+    app.listen(8000, () => {
+        console.log("server is running on port 8k");
+    });
 });
+start();
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
-import express from 'express';
-import {json, urlencoded} from 'body-parser';
-import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from "express";
+import { json, urlencoded } from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
-app.use(urlencoded({
-    extended:true
-}));
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
 app.use(json());
 
-const start =async ()=>{
-    if(!process.env.MONGO_URI) throw new Error('MONGO_URI is required');
-    mongoose.connect(process.env.MONGO_URI);
-}
-start();
-app.listen(8000,()=>{
+const start = async () => {
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is required");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (err) {
+    throw new Error("database error!");
+  }
+  app.listen(8000, () => {
     console.log("server is running on port 8k");
-})
+  });
+};
+start();
