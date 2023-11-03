7eca5705ae697240c92bb971767b5cc5efcd431f
DC-21 <cholahkuboko@gmail.com>
2023-11-03 22:59:17 +0200

    signout

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,31 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import express,{Request,Response,NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { json, urlencoded } from "body-parser";
 import mongoose from "mongoose";
-import {newPostRouter,updatePostRouter,deletePostRouter,showPostRouter,newCommentRouter,deleteCommentRouter} from './routes';
-import cors from 'cors';
-import cookieSession from 'cookie-session';
-import { currentUser, requireAuth } from '../common';
+import {
+  newPostRouter,
+  updatePostRouter,
+  deletePostRouter,
+  showPostRouter,
+
+  newCommentRouter,
+  deleteCommentRouter,
+  
+} from "./routes";
+import cors from "cors";
+import cookieSession from "cookie-session";
+import { currentUser, requireAuth } from "../common";
 
 const app = express();
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: "*",
     optionsSuccessStatus: 200,
-  }
-));
-app.set('trust proxy', true);
+  })
+);
+app.set("trust proxy", true);
 app.use(
   urlencoded({
     extended: false,
@@ -24,40 +33,44 @@ app.use(
 );
 
 app.use(json());
-app.use(cookieSession({
-  signed:false,
-  secure:false
-}));
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 app.use(currentUser);
 
 //routes
 
-app.use(requireAuth,newPostRouter);
-app.use(requireAuth,updatePostRouter);
-app.use(requireAuth,deletePostRouter);
+app.use(requireAuth, newPostRouter);
+app.use(requireAuth, updatePostRouter);
+app.use(requireAuth, deletePostRouter);
 app.use(showPostRouter);
 
-app.use(requireAuth,newCommentRouter);
-app.use(requireAuth,deleteCommentRouter);
+app.use(requireAuth, newCommentRouter);
+app.use(requireAuth, deleteCommentRouter);
 
-app.all('*',(req,res,next)=>{
-  const error = new Error('not found') as CustomError;
-  error.status=404;
+app.all("*", (req, res, next) => {
+  const error = new Error("not found") as CustomError;
+  error.status = 404;
   next();
 });
-declare global{
-  interface CustomError extends Error{
-    status?: number
+declare global {
+  interface CustomError extends Error {
+    status?: number;
   }
 }
 
 //middleware
-app.use((error: CustomError,req: Request,res: Response,next: NextFunction)=>{
-  if(error.status){
-    return res.status(error.status).json({message: error.message});
+app.use(
+  (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    if (error.status) {
+      return res.status(error.status).json({ message: error.message });
+    }
+    res.status(500).json({ message: "something went wrong" });
   }
-  res.status(500).json({message: "something went wrong"})
-})
+);
 
 //start server
 const start = async () => {
